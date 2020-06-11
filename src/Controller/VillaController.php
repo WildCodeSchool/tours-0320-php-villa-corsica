e595ca2ad84c5238a9314da068786fc7d276982d
Florian <florian-murihano_student2020@wilder.school>
2020-06-11 17:02:45 +0200

    Tarvis build correction

--- src/Controller/VillaController.php
+++ src/Controller/VillaController.php
@@ -3,10 +3,8 @@
 namespace App\Controller;
 
 use App\Entity\Villa;
-use App\Form\VillaType;
 use App\Repository\VillaRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -25,28 +23,6 @@ class VillaController extends AbstractController
         ]);
     }
 
-    /**
-     * @Route("/new", name="villa_new", methods={"GET","POST"})
-     */
-    public function new(Request $request): Response
-    {
-        $villa = new Villa();
-        $form = $this->createForm(VillaType::class, $villa);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->persist($villa);
-            $entityManager->flush();
-
-            return $this->redirectToRoute('villa_index');
-        }
-
-        return $this->render('villa/new.html.twig', [
-            'villa' => $villa,
-            'form' => $form->createView(),
-        ]);
-    }
 
     /**
      * @Route("/{id}", name="villa_show", methods={"GET"})
@@ -56,39 +32,5 @@ class VillaController extends AbstractController
         return $this->render('villa/show.html.twig', [
             'villa' => $villa,
         ]);
-    }
-
-    /**
-     * @Route("/{id}/edit", name="villa_edit", methods={"GET","POST"})
-     */
-    public function edit(Request $request, Villa $villa): Response
-    {
-        $form = $this->createForm(VillaType::class, $villa);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $this->getDoctrine()->getManager()->flush();
-
-            return $this->redirectToRoute('villa_index');
-        }
-
-        return $this->render('villa/edit.html.twig', [
-            'villa' => $villa,
-            'form' => $form->createView(),
-        ]);
-    }
-
-    /**
-     * @Route("/{id}", name="villa_delete", methods={"DELETE"})
-     */
-    public function delete(Request $request, Villa $villa): Response
-    {
-        if ($this->isCsrfTokenValid('delete'.$villa->getId(), $request->request->get('_token'))) {
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->remove($villa);
-            $entityManager->flush();
-        }
-
-        return $this->redirectToRoute('villa_index');
     }
 }
