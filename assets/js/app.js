7bfe062a1b84d6f47875d055f8af851de68055ab
ahmad <ahmadsafar257@gmail.com>
2020-06-12 16:24:01 +0200

    modifier la page d'accueil en ajoutant védio,carte,météo

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,4 +9,30 @@
 require('../scss/app.scss');
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
-// const $ = require('jquery');
+ const $ = require('jquery');
+ // weather API 
+ var city = "Corse";
+
+ $.getJSON("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=b494c2618576c2d414c7c03aabeff46b",
+  function(data){
+     console.log(data);
+ 
+     var icon = "https://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
+     var temp = data.main.temp;
+     var weather = data.weather[0].main;
+     var speed = data.wind.speed;
+     $('.icon').attr('src', icon);
+     $('.weather').append(weather); 
+     $('.temp').append(temp);
+     $('.speed').append(speed);
+  }
+ 
+ );
+ 
+ $.getJSON("https://api.windy.com/api/webcams/v2/list/country=FR?show=webcams:image,location,player&key=VaG5dg3tbtb1LZVzoRR7PDRG0ET2jaFz",
+     function(data){
+         console.log(data);
+         var webcam =  data.result.webcams[3].player.lifetime.embed;
+         $('.webcamfr').attr('src', webcam);
+     }
+ );
