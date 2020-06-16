b550330539ebe4f4d281ae235302a1899b42712b
ahmad <ahmadsafar257@gmail.com>
2020-06-16 17:07:45 +0200

    formulaire de réservation

--- src/Controller/VillaController.php
+++ src/Controller/VillaController.php
@@ -3,10 +3,17 @@
 namespace App\Controller;
 
 use App\Entity\Villa;
+use App\Form\BookingType;
 use App\Repository\VillaRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Mailer\Mailer;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\Mime\Address;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use Symfony\Component\Mailer\MailerInterface;
 
 /**
  * @Route("/villa")
@@ -25,12 +32,34 @@ class VillaController extends AbstractController
 
 
     /**
-     * @Route("/{id}", name="villa_show", methods={"GET"})
+     * @Route("/{id}", name="villa_show", methods={"GET","POST"})
      */
-    public function show(Villa $villa): Response
+    public function show(Villa $villa, Request $request, MailerInterface $mailer): Response
     {
+        $form = $this->createForm(BookingType::class);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            $booking = $form->getData();
+            $email = (new TemplatedEmail())
+                ->from($booking['email'])
+                ->to(new Address($this->getParameter('mailer_to')))
+                ->subject('Reservation')
+            // path of the Twig template to render
+                ->htmlTemplate('email/reservation.html.twig')
+           // pass l'object (booking et villa) to the template
+                ->context([
+                   'booking' => $booking,
+                   'villa'=>$villa,
+                    ]);
+            $mailer->send($email);
+            // the success flash message
+            $this->addFlash('success', 'Votre demande de réservation a été bien envoyée');
+            return $this->redirectToRoute('villa_index');
+        }
         return $this->render('villa/show.html.twig', [
             'villa' => $villa,
+            'form' => $form->createView(),
         ]);
     }
 }
---
+++ src/Form/BookingType.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Form;
+
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class BookingType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('lastname', TextType::class,['label' => 'Nom', 'constraints' => new NotBlank()])
+                ->add('firstname', TextType::class,['label' => 'Prénom','constraints' => new NotBlank()])
+                ->add('address', TextType::class,['label' => 'Adresse','constraints' => new NotBlank()])
+                ->add('email', EmailType::class,['label' => 'Email','constraints' => new NotBlank()])
+                ->add('adults', ChoiceType::class,[
+                    'label' => 'Adultes',
+                    'constraints' => new NotBlank(),
+                    'choices'  => [
+                        0,
+                        1,
+                        2,
+                        3,
+                        4,
+                        5,
+                        6,
+                    ],
+                ])
+                ->add('kids', ChoiceType::class,[
+                    'label' => 'Enfants',
+                    'choices'  => [
+                        0,
+                        1,
+                        2,
+                        3,
+                        4,
+                    ],
+                ])
+                ->add('arrive', DateType::class, [
+                    'label' => 'Arrivé',
+                    'constraints' => new NotBlank(),
+                    'widget' => 'single_text',
+                ])
+                ->add('departure', DateType::class, [
+                    'label' => 'Départ',
+                    'constraints' => new NotBlank(),
+                    'widget' => 'single_text',
+                ])
+                ;
+    }
+}
