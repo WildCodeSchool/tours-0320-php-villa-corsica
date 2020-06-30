5ab8c82a2a51d7dc137c77552025140c33e82880
Florian <florian-murihano_student2020@wilder.school>
2020-06-30 19:20:59 +0200

    recording done

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\VillaRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,12 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     /**
-     * @Route("/", name="home_index")
+     * @Route("/", name="home_index", methods={"GET"})
      */
-    public function index(): Response
+    public function index(VillaRepository $villaRepository): Response
     {
-        return $this->render('home/index.html.twig');
+        return $this->render('home/index.html.twig', [
+            'villas' => $villaRepository->findAll(),
+        ]);
     }
 }
