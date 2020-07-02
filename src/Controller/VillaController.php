7ebf0765e112274b2c60a7a8d7fd869546ca5c99
ahmad <ahmadsafar257@gmail.com>
2020-07-02 19:49:27 +0200

    second

--- src/Controller/VillaController.php
+++ src/Controller/VillaController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Villa;
 use App\Model\Booking;
 use App\Form\BookingType;
+use App\Form\VillaType;
 use App\Repository\VillaRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -33,7 +34,7 @@ class VillaController extends AbstractController
 
 
     /**
-     * @Route("/{id}", name="villa_show", methods={"GET","POST"})
+     * @Route("/{id}", name="villa_show", methods={"GET","POST"}, requirements={"id"="\d+"})
      */
     public function show(Villa $villa, Request $request, MailerInterface $mailer): Response
     {
@@ -61,5 +62,49 @@ class VillaController extends AbstractController
             'villa' => $villa,
             'form' => $form->createView(),
         ]);
+    }
+
+       /**
+     * @Route("/{id}", name="villa_delete", methods={"DELETE"})
+     */
+    public function delete(Request $request, Villa $villa): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$villa->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            unlink("../public".$villa->getPoster());
+            $entityManager->remove($villa);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('villa_index');
+    }
+
+    /**
+     * @Route("/new", name="villa_new")
+     */
+    public function new(Request $request)
+    {
+        $villa=new Villa();
+        $form=$this->createForm(VillaType::class, $villa);
+        $form->handleRequest($request);
+        if ($form->isSubmitted()&& $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $file=$form->get('posterFile')->getData();
+            $newFilename =$villa->getName();
+            $villa->setPoster("/images/".$newFilename);
+            $file->move(
+                $this->getParameter('images_directory'),
+                $newFilename
+            );
+            
+            $entityManager->persist($villa);
+            $entityManager->flush();
+             // the success flash message
+             $this->addFlash('success', 'Votre villa a été bien ajoutée ');
+             return $this->redirectToRoute('villa_index');
+        }
+
+
+        return $this->render('villa/new.html.twig', ['form'=>$form->createView(), ]);
     }
 }
