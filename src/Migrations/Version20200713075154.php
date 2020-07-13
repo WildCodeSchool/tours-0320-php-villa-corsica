7c6b5097a11349dd4a25c12083fe95dba5c7573b
Louis Marquenet <louismarquenet@hotmail.com>
2020-07-13 09:52:44 +0200

    pas fini

--- src/Migrations/Version20200713075154.php
+++ src/Migrations/Version20200713075154.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20200707152732 extends AbstractMigration
+final class Version20200713075154 extends AbstractMigration
 {
     public function getDescription() : string
     {
